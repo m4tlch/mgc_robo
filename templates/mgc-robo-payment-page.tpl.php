50fdb30a6169d0919414f22f88bf18375cd379f5
= <=>
2017-01-19 10:21:39 +0200

    payment page

---
+++ templates/mgc-robo-payment-page.tpl.php
@@ -0,0 +1,7 @@
+<div class="mgc-robo-payment-form-wrapper">
+    <div class="mgc-form-wrapper">
+      <?php
+      
+      ?>
+    </div>
+</div>
---
+++ templates/mgc-robo-set-tariff-page.tpl.php
@@ -0,0 +1,7 @@
+<div class="mgc-set-tariff-form-wrapper">
+    <div class="mgc-form-wrapper">
+      <?php
+      mgc_print_form('mgc_robo_set_tariff_form', $teacher);
+      ?>
+    </div>
+</div>
